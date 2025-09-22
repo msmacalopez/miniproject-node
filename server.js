b5e0b3fb3ac839487005a6fb9688400df0e052e1
msmacalopez <msmacalopezs@gmail.com>
2025-09-22 18:35:01 +1000

    includes explanation for JSON file api

--- server.js
+++ server.js
@@ -3,23 +3,15 @@ import express from "express";
 //6.2) get the path
 import path from "path";
 const __dirname = path.resolve();
-console.log("&&&&&&", __dirname);
+console.log("&&&&&& pwd:", __dirname);
 
 //2) define express varivle and execute the express
 const app = express();
 const PORT = 5000;
 
-//4) Response String (res.send("string")): handle the request, by default: Get
-// app.get("/", (req, res, next) => {
-//   console.log("Response sent in 4)");
-//   res.send("res.send() send response as string from Server");
-// });
-
-//5) Response as HTML String:
-// app.get("/", (req, res, next) => {
-//   console.log("Response sent in 5)");
-//   res.send("<h1>res.send() send response as Html string</h1>");
-// });
+//7) Serving static file from the public directory
+//path for static resources (keep inside public folder)
+app.use(express.static(path.join(__dirname, "/public"))); //-->go to index.html add <link>
 
 //6) send html file as response: sends index.html
 app.get("/", (req, res) => {
@@ -30,6 +22,14 @@ app.get("/", (req, res) => {
   //#####################
   //6.2) Concatenate my path with the relative path
   res.sendFile(__dirname + "/src/html/index.html");
+});
+
+//8) send JSON file now
+app.get("/api/v1/get-user", (req, res) => {
+  res.json({
+    fName: "Maca",
+    lName: "Lopez",
+  });
 });
 
 //3) open the port
