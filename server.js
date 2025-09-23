9d2d3ce078a96c08b37128ec0303d08c28d36cbe
msmacalopez <msmacalopezs@gmail.com>
2025-09-23 15:51:54 +1000

    data saved in csv file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import path from "path";
 const __dirname = path.resolve();
 console.log("&&&&&& pwd:", __dirname);
 
+//10) filesystem -> worker thread (promises, async/await)
+import fs from "fs";
+
 //2) define express varible and execute the express
 const app = express();
 const PORT = 5000;
@@ -12,6 +15,9 @@ const PORT = 5000;
 //7) Serving static file from the public directory (ej: style.css)
 //keep all static resources in public folder
 app.use(express.static(path.join(__dirname, "/public"))); //-->make changes in index.html--> add <link>
+
+//9) Middleware to receive data-body (post method)
+app.use(express.urlencoded({ extended: true }));
 
 //6) Create endpoints: send html file as response: sends index.html
 //<------Home page controller-------->
@@ -21,10 +27,32 @@ app.get("/", (req, res) => {
 });
 
 //<------User Registration controller-------->
+//can show the page and also receive if the info is sent as "get method"
 app.get("/register", (req, res) => {
+  console.log("Request QUERY: ", req.query);
   console.log("We received Registration");
   res.sendFile(path.join(__dirname, "/src/html/register.html"));
 });
+
+//Post request to receive the data sent by /register, receive data as data-body
+const fileName = "userList.csv";
+app.post("/register", (req, res) => {
+  console.log("Request.Body: ", req.body);
+  console.log("Registration completed");
+
+  const { name, email, password } = req.body;
+  const str = `${name},${email},${password}`;
+  console.log(str);
+  console.log(str.split(","));
+
+  //create file and write data
+  fs.writeFile(fileName, str, (error) => {
+    error ? console.log(error) : "Data has been written in csv file";
+  });
+
+  res.sendFile(path.join(__dirname, "/src/html/register.html"));
+});
+
 //<------User Login controller-------->
 app.get("/login", (req, res) => {
   console.log("We received Login");
